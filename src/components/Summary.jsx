b88f041f496dada8e4e2d6a387fc050164220d65
EA - Marcio Marçal Da Costa Garcia <marcio.marcal@acad.unasp.edu.br>
2023-03-07 21:17:11 -0300

    aula 07/03

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,16 +1,21 @@
 import { Rectangle } from "./Rectangle";
 
 import iconMemory from '../assets/icon-memory.svg'
+import iconReaction from '../assets/icon-reaction.svg'
+import iconVerbal from '../assets/icon-verbal.svg'
+import iconVisual from '../assets/icon-visual.svg'
 
 export function Summary() {
     return (
         <section>
             <h2>Summary</h2>
 
-            <Rectangle icon={iconMemory} type={"Reaction"} number={80} />
-            <Rectangle type={"Memory"} number={92} />
-            <Rectangle type={"Verbal"} number={61} />
-            <Rectangle type={"Visual"} number={73} />
+            <Rectangle icon={iconReaction} type={"Reaction"} number={80} />
+            <Rectangle icon={iconMemory} type={"Memory"} number={92} />
+            <Rectangle type={"Verbal"} number={61} icon={iconVerbal} />
+            <Rectangle type={"Visual"} number={73} icon={iconVisual} />
+
+            <button>Continue</button>
         </section>
     )
 }
